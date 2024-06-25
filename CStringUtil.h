b51857a529c5f8ee5d39545b23b957791f43d232
Narutaka <narutaka@minamide.org>
2024-06-25 10:33:52 +0900

    Update CStringUtil.h

--- CStringUtil.h
+++ CStringUtil.h
@@ -2,7 +2,7 @@
 class CStringUtil
 {
 public:
-	int GetLength(CStringA &strString);
+	int GetLength(CStringA& strString);
 	int GetLength(CStringW& strString);
 	CStringA Mid(CStringA& strString, int nStart, int nEnd =0);
 	CStringW Mid(CStringW& strString, int nStart, int nEnd =0);
