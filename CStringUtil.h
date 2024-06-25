9cb1ba225c284d85441bb0b37134b2a448f43e87
Narutaka <narutaka@minamide.org>
2024-06-25 10:36:06 +0900

    Update CStringUtil.h

--- CStringUtil.h
+++ CStringUtil.h
@@ -6,4 +6,6 @@ public:
 	int GetLength(CStringW& strString);
 	CStringA Mid(CStringA& strString, int nStart, int nEnd =0);
 	CStringW Mid(CStringW& strString, int nStart, int nEnd =0);
+
+	CStringA escape5c(CStringW& strString);
 };
